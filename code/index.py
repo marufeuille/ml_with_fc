c5824922617e761d4c65bc45fe7fa3dfbe0c93ef
muffy <marufeuille@gmail.com>
2020-05-26 10:28:56 +0900

    remove comments

--- code/index.py
+++ code/index.py
@@ -12,8 +12,6 @@ def handler(environ, start_response):
     with open("./model/creditcard_fraud_detection_model.pkl", "rb") as f:
         lr = pickle.load(f)
 
-    context = environ['fc.context']
-    request_uri = environ['fc.request_uri']
     request_method = environ['REQUEST_METHOD']
     try:
         request_body_size = int(environ.get('CONTENT_LENGTH', 0))
@@ -21,7 +19,6 @@ def handler(environ, start_response):
     except (ValueError):
         request_body_size = 0
     if request_method == 'POST':
-        #logger.info(request_body)
         print(request_body.decode("utf-8"))
         js = json.loads(request_body.decode("utf-8"))
         X = [js["Time"]]
@@ -32,7 +29,6 @@ def handler(environ, start_response):
         response = str(preds[0,1]).encode("utf-8")
     else:
         response = b'Hello World!!'
-    # do something here
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
